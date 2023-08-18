d5f0f80cef9310570208c4f4222589a129f82f4e
paul-tes <pauasca8462@gmail.com>
2023-08-18 09:31:02 +0300

    Add callbacks to Post, Like, and Comment classes as per a feedback

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,15 @@ class Post < ActiveRecord::Base
   has_many :likes, class_name: 'Like', foreign_key: :post_id
   has_many :comments, foreign_key: :post_id
 
-  def update_user_posts_counter
-    author.increment!(posts_counter)
-  end
+  after_save :update_user_posts_counter
 
   def recent_comment
     comments.limit(5).order(created_at: :asc)
+  end
+
+  private
+
+  def update_user_posts_counter
+    author.increment!(posts_counter)
   end
 end
