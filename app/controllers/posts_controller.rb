74de0edaf4e5242536e6f9b2d338e93e6b7d5b80
paul-tes <pauasca8462@gmail.com>
2023-08-24 18:32:57 +0300

    Add create action for current user in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,30 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = Post.where(author_id: params[:user_id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(posts_params)
+    @post.author = current_user
+    if @post.save
+      redirect_to user_posts_path(id: @post.author_id)
+    else
+      render :new, alert: 'ERROR! something went wrong while creating the post'
+    end
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:title, :text)
   end
 end
