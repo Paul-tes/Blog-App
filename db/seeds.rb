3a691bcd1f45df177842392bff33a34fb787fde2
paul-tes <pauasca8462@gmail.com>
2023-08-24 08:56:57 +0300

    Add sample datas putes in db/seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# create users sample data
+
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+second_user = User.create(name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+User.create(name: 'Davied', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Student from Ethiopia.')
+
+# create sample posts data
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author: first_user, title: 'Hello', text: 'This is my new post')
+Post.create(author: first_user, title: 'Hello', text: 'This is my fourth post')
+Post.create(author: first_user, title: 'Hello', text: 'This is my second post')
+
+# create sample comment data
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+Comment.create(post: second_post, author: second_user, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+Comment.create(post: second_post, author: second_user, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
