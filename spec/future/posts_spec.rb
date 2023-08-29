03d458bb5df88569263a47bc707c76e85f9a837b
paul-tes <pauasca8462@gmail.com>
2023-08-29 22:10:20 +0300

    Add Post#show test futures cases

--- spec/future/posts_spec.rb
+++ spec/future/posts_spec.rb
@@ -38,7 +38,65 @@ RSpec.feature 'Posts', type: :feature do
       expect(page).to have_content('Hello, two')
       expect(page).to have_content('Hello, three')
     end
-end
-  
-end
 
+    it 'should some of the posts body' do
+      expect(page).to have_content(@first_post.text.truncate_words(5))
+    end
+
+    it 'should display the first comments on a post' do
+      expect(page).to have_content('This is the 1st comment on the last post')
+      expect(page).to have_content('This is the 2nd comment on the last post')
+      expect(page).to have_content('This is the 3rd comment on the last post')
+    end
+
+    it 'how many comments a post has.' do
+      expect(page).to have_content('Comments: 3')
+    end
+
+    it 'how many likes a post has.' do
+      expect(page).to have_content('Likes: 0')
+    end
+
+    it 'should show a section for pagination if there are more posts than fit on the view' do
+      expect(page).to have_content('Pagination')
+    end
+
+    it "should redirect to that post's show page when clicked" do
+      click_link(@first_post.title)
+      expect(current_path).to eq(user_post_path(@user, @first_post))
+    end
+  end
+
+  describe 'post#show' do
+    before(:each) do
+      visit user_post_path(@user, @last_post)
+    end
+    it 'should show post\'s title' do
+      expect(page).to have_content(@last_post.title)
+    end
+
+    it 'how many comments a post has.' do
+      expect(page).to have_content("Comments: #{@last_post.comments_counter}")
+    end
+
+    it 'how many likes a post has' do
+      expect(page).to have_content("Likes: #{@last_post.likes_counter}")
+    end
+
+    it 'should some of the posts body' do
+      expect(page).to have_content(@last_post.text)
+    end
+
+    it "should show the comment's author" do
+      expect(page).to have_content(@comment1.author.name)
+      expect(page).to have_content(@comment2.author.name)
+      expect(page).to have_content(@comment3.author.name)
+    end
+
+    it "should show the comment's for each user" do
+      @user2.comments.each do |comment|
+        expect(page).to have_content(comment.text)
+      end
+    end
+  end
+end
