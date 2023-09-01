5b9acffb41763f93405bd60d1091adf874568137
paul-tes <pauasca8462@gmail.com>
2023-09-01 15:57:08 +0300

    Error on models and role assignes fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,6 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def logout
-    sign_out current_user
-    redirect_to root_path
-  end
-
   def show
     @user = User.find(params[:id])
   end
