d9b6707143265adcbeb135cfe7191b9b5e5c7a00
paul-tes <pauasca8462@gmail.com>
2023-09-01 21:11:19 +0300

    Revert changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,14 +2,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # Guest user (not logged in)
+    # Define abilities for the user here. For example:
+    #
+    return unless user.present?
 
-    if user.role == 'admin'
-      can :manage, :all # admin can do all things
-    else
-      can :read, :all
-      can :manage, Comment, user_id: user.id
-      can :manage, Post, user_id: user.id
-    end
+    can :manage, Comment, author_id: user.id
+    can :manage, Post, author_id: user.id
+    can :read, :all
+    return unless user.role == 'admin'
+
+    can :manage, :all
   end
 end
